2778d77f3f700ac215b7f274cae42259643cbbdc
Erik Osterman <erik@cloudposse.com>
2018-12-28 09:31:57 -0800

    Address CR

--- modules/docker-build/variables.tf
+++ modules/docker-build/variables.tf
@@ -12,7 +12,10 @@ variable "dockerfile" {
   default = "Dockerfile"
 }
 
+# NOTE: this variable won't actually be used for anything and the actual `depends_on` keyword 
+# in terraform does not support interpolation.
 variable "depends_on" {
   type    = "list"
+  description = "Define a list of variables that this module depends on in order to force serialized execution."
   default = []
 }
--- modules/init-dirs/variables.tf
+++ modules/init-dirs/variables.tf
@@ -5,7 +5,10 @@ variable "dirs" {
   default = ["", "conf", "rootfs"]
 }
 
+# NOTE: this variable won't actually be used for anything and the actual `depends_on` keyword 
+# in terraform does not support interpolation.
 variable "depends_on" {
   type    = "list"
+  description = "Define a list of variables that this module depends on in order to force serialized execution."
   default = []
 }
